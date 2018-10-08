6269a91f78f9b1156f22e7a66cbeef43268f7506
Raffique <Raffique@users.noreply.github.com>
2018-10-08 05:20:00 -0900

    Add files via upload

---
+++ Drone_reciever.ino
@@ -0,0 +1,208 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const uint64_t pipeIn = 0xE8E8F0F0E1LL;     //Remember that this code is the same as in the transmitter
+RF24 radio(9, 10);  //CSN and CE pins
+
+// The sizeof this struct should not exceed 32 bytes
+struct Received_data {
+  byte up = 1; //up
+  byte right; //right
+  byte down; //down
+  byte left; //left
+  
+  byte YB; //Y
+  byte BB; //B
+  byte AB; //A
+  byte XB; //X
+  
+  byte L1B; //L1
+  byte L2B; //L2
+  byte R1B; //R1
+  byte R2B; //R2
+  
+  byte L3B; //L3
+  byte LX = 127; //LX
+  byte LY = 127; //LY
+  
+  byte R3B; //R3
+  byte RX =127; //RX
+  byte RY = 0; //RY
+  
+  byte select; //SELECT
+  byte start; //START
+  byte main; //HOME
+};
+
+Received_data received_data;
+
+
+boolean income = false;
+unsigned long lastRecTime = 0;
+
+
+
+//////////////////////CONFIGURATION///////////////////////////////
+#define CHANNEL_NUMBER 12  //set the number of chanels
+#define CHANNEL_DEFAULT_VALUE 1500  //set the default servo value
+#define FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PULSE_LENGTH 300  //set the pulse length
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 7  //set PPM signal output pin on the arduino
+
+/*this array holds the servo values for the ppm signal
+ change theese values in your code (usually servo values move between 1000 and 2000)*/
+int ppm[CHANNEL_NUMBER];
+
+////////////////////////////SUB FUNCTIONS/////////////////////////////////////////////////
+void reset_data() 
+{
+  // 'safe' values to use when NO radio input is detected
+  //received_data.up = 0;      
+  received_data.down = 0;
+  received_data.right = 0;
+  received_data.left = 0;
+  
+  received_data.AB = 0;
+  received_data.BB = 0;
+  received_data.YB = 0;
+  received_data.XB = 0;
+  
+  received_data.L1B = 0;
+  received_data.L2B = 0;
+  received_data.R1B = 0;
+  received_data.R2B = 0;
+  
+  received_data.R3B = 0;
+  received_data.RX = 127;
+  //received_data.RY = 0;
+  
+  received_data.L3B = 0;
+  received_data.LX = 127;
+  received_data.LY = 127;
+  
+  received_data.select = 0;
+  received_data.start = 0;
+  received_data.main = 0;
+}
+
+/*******************************************************************/
+
+//We create the function that will read the data each certain time
+void receive_data()
+{
+  while ( radio.available() ) {
+  radio.read(&received_data, sizeof(Received_data));
+  lastRecTime = millis(); //Here we receive the data
+  income = true;
+}
+}
+/**********************************************************************/
+int flightmodes(){
+  int modeVal;
+
+  if(received_data.up == 1){modeVal = 1200;}
+  else if(received_data.up == 2){modeVal = 1300;}
+  else if(received_data.up == 3){modeVal = 1400;}
+  else if(received_data.up == 4){modeVal = 1600;}
+  else if(received_data.up == 5){modeVal = 1700;} 
+  else {modeVal = 1800;}
+  return modeVal;
+  
+}
+
+///////////////////////////////////MAIN SETUP AND LOOP/////////////////////////////
+void setup(){  
+  Serial.begin(115200);
+  //initiallize default ppm values
+  for(int i=0; i<CHANNEL_NUMBER; i++){
+      ppm[i]= CHANNEL_DEFAULT_VALUE;
+  }
+
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+  
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+
+  ///////////////////nrf2401 SETTINGS//////////////////////
+   //We reset the received values
+  reset_data();
+  received_data.up = 1;
+  
+  //Once again, begin and radio configuration
+  radio.begin();
+  //radio.setPALevel(RF24_PA_LOW);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);  
+  radio.openReadingPipe(1,pipeIn);
+  
+  //We start the radio comunication
+  radio.startListening();
+
+}
+
+void loop(){
+  
+  //Receive the radio data
+  receive_data();
+
+  if(millis()- lastRecTime > 1000){
+    
+    }
+  
+  if(income == true){
+    ppm[0] = map(received_data.LX,0,255,1000,2000); Serial.print("roll: ");Serial.println(ppm[0]);
+    ppm[1] = map(received_data.LY,0,255,1000,2000); Serial.print("pitch: ");Serial.println(ppm[1]);
+    ppm[2] = map(received_data.RY,0,255,1000,2000); Serial.print("throttle: ");Serial.println(ppm[2]);
+    ppm[3] = map(received_data.RX,0,255,1000,2000); Serial.print("yaw: ");Serial.println(ppm[3]);
+    ppm[4] = flightmodes();Serial.println(ppm[4]);
+    ppm[5] = map(received_data.right,0,1,1000,2000); Serial.print("aux6: ");Serial.println(ppm[5]);
+    ppm[6] = map(received_data.left,0,1,1000,2000); Serial.print("aux7: ");Serial.println(ppm[6]);
+    ppm[7] = map(received_data.down,0,1,1000,2000); Serial.print("aux8: ");Serial.println(ppm[7]);
+      }
+      
+    reset_data();
+    lastRecTime = millis();
+    income = false;
+  
+}
+
+/////////////////////////////////TIMER EVENTS///////////////////////////////////////
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static boolean state = true;
+  
+  TCNT1 = 0;
+  
+  if (state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PULSE_LENGTH * 2;
+    state = false;
+  } else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= CHANNEL_NUMBER){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PULSE_LENGTH;// 
+      OCR1A = (FRAME_LENGTH - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PULSE_LENGTH) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
---
+++ Drone_transmitter.ino
@@ -0,0 +1,227 @@
+/*
+   Can comment out all the serial.print commands, they are there just for testing resutls 
+   PA level can be increaed by uncommenting the command setPALevel command and chnagong the input from RF_PA_LOW to RF_PA_HIGH
+   code can be altered from sending only when but is pressed to continuously send button state
+*/
+
+#include <XBOXUSB.h>
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+USB Usb;
+XBOXUSB Xbox(&Usb);
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+const uint64_t my_radio_pipe = 0xE8E8F0F0E1LL; //Remember that this code should be the same for the receiver
+#define CE 9
+#define CSN 53
+RF24 radio(CE, CSN);  //Set CE and CSN pins
+
+// The sizeof this struct should not exceed 32 bytes
+struct Data_to_be_sent {
+  byte up; //up
+  byte right; //right
+  byte down; //down
+  byte left; //left
+  
+  byte YB; //Y
+  byte BB; //B
+  byte AB; //A
+  byte XB; //X
+  
+  byte L1B; //L1
+  byte L2B; //L2
+  byte R1B; //R1
+  byte R2B; //R2
+  
+  byte L3B; //L3
+  byte LX; //LX
+  byte LY; //LY
+  
+  byte R3B; //R3
+  byte RX; //RX
+  byte RY; //RY
+  
+  byte select; //SELECT
+  byte start; //START
+  byte main; //HOME
+};
+
+
+//Create a variable with the structure above and name it sent_data
+Data_to_be_sent sent_data;
+
+boolean changes = false;
+int threshold = 100;
+unsigned long lastTime = millis();
+int StickPos = 0;
+unsigned long timing = 0;
+
+/////////Sub functions/////////////////////
+int Throttle(){
+  if (Xbox.getAnalogHat(RightHatY) > threshold || Xbox.getAnalogHat(RightHatY) < -threshold) {
+      
+        int data = Xbox.getAnalogHat(RightHatY);
+        if(data >= 0){ 
+            timing = map(data, 0, 32767, 61, map(data, 0, 32767, 25, 1));
+        }
+        else{
+            timing = map(data, -1, -32768, 61, map(data, -1, -32768, 25, 1));
+        }
+        if(millis() - lastTime > timing){ 
+      
+         if(data >= 0){
+            StickPos += 1;
+         }
+         else{
+            StickPos -= 1;
+         }
+          
+        if (StickPos > 255){
+          StickPos = 255;
+        }
+        if (StickPos < 0){
+          StickPos = 0;
+        }
+    
+        lastTime = millis();
+        return StickPos;
+        }
+      } 
+}
+
+void reset_data(){
+ //Reset each channel value
+  //sent_data.up = 0; // used to control flight mode, keep value
+  sent_data.down = 0; 
+  sent_data.right = 0;
+  sent_data.left = 0;
+  
+  sent_data.AB = 0;
+  sent_data.BB = 0;
+  sent_data.XB = 0;
+  sent_data.YB = 0;
+  
+  sent_data.L1B = 0;
+  //sent_data.L2B = 0;
+  sent_data.R1B = 0;
+  //sent_data.R2B = 0;
+  
+  sent_data.L3B = 0;
+  //sent_data.LX = 127; //neutral value for loaded spring joystick
+  //sent_data.LY = 127; //neutral value for loaded spring joystick
+  
+  sent_data.R3B = 0;
+  //sent_data.RX = 127; //neutral value for loaded spring joystick
+  //sent_data.RY = 0; //this is used for throttle which should stay constant on the drone
+  
+  sent_data.select = 0;
+  sent_data.start = 0;
+  sent_data.main = 0;
+}
+
+void setup()
+{ 
+  Serial.begin(115200);
+
+  #if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+  #endif
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); //halt
+  }
+  Serial.println(F("\r\nXBOX USB Library Started"));
+
+  radio.begin();
+  //radio.setPALevel(RF24_PA_LOW);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(my_radio_pipe);
+
+  reset_data();
+  
+}
+
+/**************************************************/
+
+
+void loop()
+{
+
+  Usb.Task();
+  if (Xbox.Xbox360Connected) {
+
+      sent_data.LX = map(Xbox.getAnalogHat(LeftHatX), -32768, 32767, 0, 255);
+      sent_data.LY = map(Xbox.getAnalogHat(LeftHatY), -32768, 32767, 0, 255);
+      sent_data.RX = map(Xbox.getAnalogHat(RightHatX), -32768, 32767, 0, 255);
+      sent_data.RY = Throttle(); Serial.println(sent_data.RY);
+      sent_data.L2B = Xbox.getButtonPress(L2);
+      sent_data.R2B = Xbox.getButtonPress(R2);
+      
+      
+    if (Xbox.getButtonClick(UP)) {
+      sent_data.up++;
+      if (sent_data.up > 6){
+          sent_data.up = 1;
+        }
+    }
+    if (Xbox.getButtonClick(DOWN)) {
+      sent_data.down = 1;
+    }
+    if (Xbox.getButtonClick(LEFT)) {
+      sent_data.left = 1;
+    }
+    if (Xbox.getButtonClick(RIGHT)){
+      sent_data.right = 1;
+    }
+
+    if (Xbox.getButtonClick(START)){
+      sent_data.start = 1; 
+    }
+    if (Xbox.getButtonClick(BACK)){
+      sent_data.select = 1;
+    }
+    if (Xbox.getButtonClick(L3)){
+      sent_data.L3B = 1;
+    }
+    if (Xbox.getButtonClick(R3)){
+      sent_data.R3B = 1;
+    }
+    if (Xbox.getButtonClick(L1)){
+      sent_data.L1B = 1;
+    }
+    if (Xbox.getButtonClick(R1)){
+      Serial.println(F("R1"));
+      sent_data.R1B = 1;
+    }
+    if (Xbox.getButtonClick(XBOX)){
+      sent_data.main = 1;
+    }
+
+    if (Xbox.getButtonClick(A)){
+      sent_data.AB = 1;
+    }
+    if (Xbox.getButtonClick(B)){
+      sent_data.BB = 1;
+    }
+    if (Xbox.getButtonClick(X)){
+      sent_data.XB = 1;
+    }
+    if (Xbox.getButtonClick(Y)){
+      sent_data.YB = 1;
+    }
+  }
+  
+  ///////////////////////////////////////////////////
+  ///////   SENDING DATA WITH THIS COMMAND      /////
+  radio.write(&sent_data, sizeof(Data_to_be_sent));
+  reset_data();
+  
+}
+
+
+  
